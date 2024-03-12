0b9bf4709b1167f47f32d835f5a80a2049f97cf6
manc1n1 <mancinij1111@gmail.com>
2024-03-12 06:37:48 -0400

    Add setActivity.js event

---
+++ events/setActivity.js
@@ -0,0 +1,34 @@
+const { Events, ActivityType } = require('discord.js');
+
+let currentIndex = 0;
+const states = [
+	{
+		type: ActivityType.Custom,
+		name: '0',
+		state: '🥪 You look hungry to me.',
+	},
+	{
+		type: ActivityType.Custom,
+		name: '1',
+		state: '🥪 I tell you what -',
+	},
+	{
+		type: ActivityType.Custom,
+		name: '2',
+		state: '🥪 have a Discord Role on me.',
+	},
+];
+
+module.exports = {
+	name: Events.ClientReady,
+	execute(client) {
+		function setActivitySequentially() {
+			const state = states[currentIndex];
+			client.user.setActivity(state);
+			console.log(state);
+			currentIndex = (currentIndex + 1) % states.length;
+			setTimeout(setActivitySequentially, 5000);
+		}
+		setActivitySequentially();
+	},
+};
