6ab6555e7b0576f2998af62c0aaaa49512d97e6d
manc1n1 <mancinij1111@gmail.com>
2024-03-16 00:59:32 -0400

    Update getRole.js utility command

--- commands/utility/getRole.js
+++ commands/utility/getRole.js
@@ -1,56 +1,54 @@
 const { SlashCommandBuilder } = require('discord.js');
 const hiscores = require('osrs-json-hiscores');
 
-let rank = 0;
-let totalLvl = 0;
+class CustomError extends Error {
+	constructor(message) {
+		super(message);
+	}
+}
 
 function sanitizeRSN(rsn) {
 	return rsn.replace(/[ _-]/g, '_').toLowerCase();
 }
 
+let rank, totalLvl;
 async function fetchHiscores(rsn) {
 	const res = await hiscores.getStats(rsn);
 	rank = res.main.skills.overall.rank;
 	totalLvl = res.main.skills.overall.level;
 }
 
-function determineRole(total) {
-	if (total == 2277) {
-		return '1070071993845694485';
-	} else if (total > 2199) {
-		return '1070072215732764742';
-	} else if (total > 2099) {
-		return '1070072519148716154';
-	} else if (total > 1999) {
-		return '1070072837576069252';
-	} else if (total > 1749) {
-		return '1070073018853900389';
-	} else if (total > 1499) {
-		return '1070073226807476295';
-	} else if (total > 1249) {
-		return '1070073450787516577';
-	} else if (total > 999) {
-		return '1070073537068535858';
-	} else {
-		return;
-	}
-}
+const roleMapping = {
+	2277: '1070071993845694485',
+	2199: '1070072215732764742',
+	2099: '1070072519148716154',
+	1999: '1070072837576069252',
+	1749: '1070073018853900389',
+	1499: '1070073226807476295',
+	1249: '1070073450787516577',
+	999: '1070073537068535858',
+};
 
 async function removeOtherRoles(member, total) {
-	const roleMapping = {
-		'1070071993845694485': 2277,
-		'1070072215732764742': 2199,
-		'1070072519148716154': 2099,
-		'1070072837576069252': 1999,
-		'1070073018853900389': 1749,
-		'1070073226807476295': 1499,
-		'1070073450787516577': 1249,
-		'1070073537068535858': 999,
-	};
-	const rolesToRemove = Object.entries(roleMapping)
-		.filter(([, condition]) => condition <= total)
-		.map(([role]) => role);
+	const rolesToRemove = [];
+	for (const [condition, roleId] of Object.entries(roleMapping)) {
+		if (condition <= total) {
+			rolesToRemove.push(roleId);
+		}
+	}
 	await member.roles.remove(rolesToRemove);
+}
+
+function determineRole(total) {
+	const roleKeys = Object.keys(roleMapping).sort((a, b) => b - a);
+	for (let i = 0; i < roleKeys.length; i++) {
+		if (total >= roleKeys[i]) {
+			return roleMapping[roleKeys[i]];
+		}
+	}
+	throw new CustomError(
+		'No role assigned, need at least **Total Level: 1000**',
+	);
 }
 
 module.exports = {
@@ -77,8 +75,6 @@ module.exports = {
 			await interaction.followUp(
 				`*RSN:* **${rsn}**\n*Overall Rank:* **${rank}**\n*Total Level:* **${totalLvl}**\n*Role:* <@&${roleToAdd}>`,
 			);
-			totalLvl = 0;
-			rank = 0;
 		} catch (err) {
 			console.error(`Error fetching hiscores: ${err.message}`);
 			await interaction.followUp(
